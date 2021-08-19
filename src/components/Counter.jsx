44d75a91e585ce21c035ad46951a9c12b0d8ee16
arahimov <artemiirakhimov@yandex.ru>
2021-08-20 00:30:26 +0300

    Создает компонент Counter, добавляет обработчики событий

---
+++ src/components/Counter.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(1);
+  const formCount = () => (count === 0 ? "Ноль" : count);
+
+  const getBageClasses = () => {
+    let classes = "badge m-2 bg-";
+    classes += count === 0 ? "danger" : "primary";
+    return classes;
+  };
+
+  const tags = ["tag1", "tag2", "tag3"];
+
+  const renderTags = () => {
+    if (tags.length === 0) return "Тегов не найдено";
+    return tags.map(tag => <li key={tag}>{tag}</li>);
+  };
+
+  const handleIncrement = productId => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = productId => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <>
+      <ul>{renderTags()}</ul>
+      <span className={getBageClasses()}>{formCount()}</span>
+      <button
+        onClick={() => handleIncrement({ id: 1 })}
+        className="btn btn-secondary btn-sm"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => handleDecrement({ id: 1 })}
+        className="btn btn-secondary btn-sm"
+      >
+        Decrement
+      </button>
+    </>
+  );
+};
+
+export default Counter;
